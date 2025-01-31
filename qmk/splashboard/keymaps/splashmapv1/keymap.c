fad8b71bbb2e1f03177e427f50f31aa1f385fcf5
Florian Näf <florian.naef@protonmail.com>
2025-01-31 13:24:45 +0100

    add minor keymap changes

--- qmk/splashboard/keymaps/splashmapv1/keymap.c
+++ qmk/splashboard/keymaps/splashmapv1/keymap.c
@@ -2,22 +2,22 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /*
-     * Layer 0 is in a first step mathing my lenovo keyboard
+     * 
      */
     [0] = LAYOUT_8x5(
         // LEFT
         KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,
-        _______, _______, KC_Q,    KC_W,    KC_E,    KC_R,
-        _______, _______, KC_A,    KC_S,    KC_D,    KC_F,
-        _______, _______, KC_Y,    KC_X,    KC_C,    KC_V,
-        _______, _______, _______, KC_LCTL, KC_SPC,  KC_LGUI,
+        XXXXXXX, XXXXXXX, KC_Q,    KC_W,    KC_E,    KC_R,
+        XXXXXXX, XXXXXXX, KC_A,    KC_S,    KC_D,    KC_F,
+        XXXXXXX, XXXXXXX, KC_Y,    KC_X,    KC_C,    KC_V,
+        XXXXXXX, XXXXXXX, XXXXXXX, KC_LCTL, KC_SPC,  KC_LGUI,
 
         // RIGHT
         KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,
         KC_T,    KC_Z,    KC_U,    KC_I,    KC_O,    KC_P,
         KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
         KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
-        KC_TAB,  LT(1, KC_SPC), KC_ESC, _______, _______, _______
+        KC_TAB,  LT(1, KC_SPC), KC_ESC, XXXXXXX, XXXXXXX, XXXXXXX
     ),
 
     [1] = LAYOUT_8x5(
@@ -27,7 +27,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______,
-
 
         // RIGHT
         _______, _______, _______, _______, _______, _______,
